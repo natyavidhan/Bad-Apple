f47ce01ced7f232fb8bfdbe5598bf5ddd1d04733
studiousgamer <natyavidhanbiswas10@gmail.com>
2022-06-16 01:00:55 +0530

    way more optimized way of loading frames

--- main.py
+++ main.py
@@ -1,59 +1,100 @@
 import matplotlib.pyplot as plt
 from PIL import Image
 import numpy as np
+import cv2
+
 import json, os, time
 
 RES = 9
 
-frames = []
+frames = ""
+
 
 def load():
+    global frames
     start = time.time()
-    for i in range(6572):
-        x, y = [],[]
-        img = Image.open(f"src/frames/frame{i}.png")
-        img = img.resize((960//RES,720//RES))
-        data = img.getdata()
+    
+    vid = cv2.VideoCapture("src/Bad Apple.mp4")
+    success, image = vid.read()
+    count = 0
+    while success:
+        frame_data = ""
+        line_data = []
+        image = Image.fromarray(np.uint8(image))
+        
+        image = image.resize((960//RES,720//RES))
+        data = image.getdata()
         data = np.array(data)
+        curr_line_data = ""
+        curr_line = 0
         for index, value in enumerate(data):
-            x_ = index % (960//RES)
-            y_ = (720//RES) - index // (960//RES)
+            line = index // (960//RES)
+            if line != curr_line:
+                line_data.append(curr_line_data)
+                curr_line_data = ""
+                curr_line = line
             if value[0] == 0:
-                x.append(x_)
-                y.append(y_)
-        frames.append({'x':x,'y':y})
-        print(i)
-    print(f"Loaded {len(frames)} frames in {time.time()-start} seconds")
+                curr_line_data += "1"
+            else:
+                curr_line_data += "0"
+        line_data.append(curr_line_data)
+
+        for line in line_data:
+            sudo_line_data = ""
+            curr_color = 1
+            curr_color_count = 0
+            for px in line:
+                if curr_color == int(px):
+                    curr_color_count += 1
+                else:
+                    sudo_line_data += f"{curr_color} {curr_color_count},"
+                    curr_color = int(px)
+                    curr_color_count = 1
+            sudo_line_data += f"{curr_color} {curr_color_count}"
+            frame_data += sudo_line_data + "|"
+
+        frame_data = frame_data[:-1]
+        
+        frames += f"{frame_data}\n"
+        print(f"Frame {count+1} Extracted")
+
+        success, image = vid.read()
+        count += 1
+        
+    print(f"Loaded {count+1} frames in {time.time()-start} seconds")
     start = time.time()
-    json.dump(frames, open("src/frames.json", "w"))
+    with open("src/frames.txt", "w") as f:
+        f.write(frames)
     print(f"Dumped frames data in {time.time()-start} seconds")
 
-print("Loading frames...")
-if os.path.isfile("src/frames.json"):
-    print("Frames data found, loading...")
-    start = time.time()
-    frames = json.load(open("src/frames.json"))
-    print(f"Loaded {len(frames)} frames in {time.time()-start} seconds")
-else:
-    print("Frames data not found, loading from images...")
+
+def start_animation():
+    plt.ion()
+    fig, ax = plt.subplots()
+    x, y = [],[]
+    sc = ax.scatter(x,y, s=15, c='black')
+    plt.xlim(0,960//RES)
+    plt.ylim(0,720//RES)
+    plt.draw()
+
+    for index, frame in enumerate(frames):
+        x, y = frame['x'], frame['y']
+        sc.set_offsets(np.c_[x,y])
+        fig.canvas.draw_idle()
+        plt.pause(0.006)
+
+    plt.waitforbuttonpress()
+
+def main():
+    print("Loading frames...")
+    if os.path.isfile("src/frames.json"):
+        print("Frames data found, loading...")
+        start = time.time()
+        frames = json.load(open("src/frames.json"))
+        print(f"Loaded {len(frames)} frames in {time.time()-start} seconds")
+    else:
+        print("Frames data not found, loading from images...")
+        load()
+
+if __name__ == "__main__":
     load()
-
-
-plt.ion()
-fig, ax = plt.subplots()
-x, y = [],[]
-sc = ax.scatter(x,y, s=15, c='black')
-plt.xlim(0,960//RES)
-plt.ylim(0,720//RES)
-plt.draw()
-
-
-for index, frame in enumerate(frames):
-    x, y = frame['x'], frame['y']
-    sc.set_offsets(np.c_[x,y])
-    fig.canvas.draw_idle()
-    plt.pause(0.006)
-
-
-
-plt.waitforbuttonpress()
